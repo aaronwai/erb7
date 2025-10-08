df39a23ea4b10a89b985ab49678f2b8ed4258934
aaronwai <aaron.lung.wai@gmail.com>
2025-10-08 17:55:15 +0800

    Step 14 : choices selection

--- config/settings.py
+++ config/settings.py
@@ -6,7 +6,7 @@ load_dotenv()
 BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.getenv('SITE_SECRET_KEY')
 DEBUG = True
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["localhost","127.0.0.1"]
 
 # Application definition
 DJANGO_APP = [ 'django.contrib.admin',
